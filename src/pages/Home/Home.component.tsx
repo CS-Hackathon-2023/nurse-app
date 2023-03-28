973c1d452157a8ee0c490cd82d0ec45f996cbf4f
Liad Peretz <liad.per@gmail.com>
2023-03-28 10:31:30 +0300

    Updates home page

--- src/pages/Home/Home.component.tsx
+++ src/pages/Home/Home.component.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { app } from "../../base";
 
 export function Home (): JSX.Element {
   return (
     <div>
         <h1>Home</h1>
-        <button onClick={()=> app.auth().signOut()}>Click me</button>
     </div>
   );
 }
