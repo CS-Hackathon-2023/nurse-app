610a94dd81050998249044558f5298d5a15b1b60
Liad Peretz <liad.per@gmail.com>
2023-03-28 10:31:43 +0300

    Adds router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 
-import './App.css'
-import React from 'react'
-import { usePolling } from './hooks/usePolling'
+import './App.css';
+import React from 'react';
+// import { usePolling } from './hooks/usePolling';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home/Home.component';
+import { Login } from './pages/Login/Login.component';
+import { SignUp } from './pages/SignUp/SignUp.component';
 
 function App (): JSX.Element {
-  const url = 'https://jsonplaceholder.typicode.com/todos/1'
-  const pollingInterval = 15000 // 15 seconds in milliseconds
+  // const url = 'https://jsonplaceholder.typicode.com/todos/1';
+  // const pollingInterval = 15000; // 15 seconds in milliseconds
 
-  const response = usePolling(url, pollingInterval)
-  if (response == null) {
-    return <p>Loading...</p>
-  }
+  // const response = usePolling(url, pollingInterval);
+  // if (response == null) {
+  //   return <p>Loading...</p>;
+  // }
 
-  if (response.error) {
-    return <p>Error: {response.error.message}</p>
-  }
+  // if (response.error) {
+  //   return <p>Error: {response.error.message}</p>;
+  // }
 
   return (
-    <div>
-      <p>Status: {response.status.toString()}</p>
-      <p>Data: {JSON.stringify(response.data)}</p>
-    </div>
-  )
+    <Router>
+      <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/login" Component={Login} />
+          <Route path="/signup" Component={SignUp} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
